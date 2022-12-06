c2ffa2e9c970b9acfd12e02789441d858a50ae1a
Hiwot Woldesemayat <hiwot.woldesemayat@fdmgroup.com>
2022-12-06 14:29:36 -0500

    updated array

--- component-example/src/App.jsx
+++ component-example/src/App.jsx
@@ -40,40 +40,39 @@ function YoutubeComponent(props){
 
 function App() {
  
-  const listOfViews = [
-    {
-      thumbnailImg:"https://via.placeholder.com/600x400",
-      videoTitle:"Why a cheesburger?",
-      uploader:"Mark",
-      videouploaded:"11/30/2022",
-      views:10000 + " views"
+let sampleData = [
+  {
+    thumbnailImg: "https://via.placeholder.com/600x400",
+    videoTitle: "He did WHAT?!?! (My intial reaction)",
+    videoUploader: "MaxTongster",
+    dateVideoUploaded: "10-31-2022",
+    videoViews: 10
   },
   {
-    thumbnailImg:"https://via.placeholder.com/600x400",
-    videoTitle:"Luis' big break",
-    uploader:"Luis",
-    videouploaded:"11/30/2022",
-    views:20000 + " views"
-},
-{
-  thumbnailImg:"https://via.placeholder.com/600x400",
-  videoTitle:"How to make a taco",
-  uploader:"CookingFoods",
-  videouploaded:"11/30/2022",
-  views:30000 + " views"
-}
-
-]
+    thumbnailImg: "https://via.placeholder.com/600x400",
+    videoTitle: "I bought my best friend a Tesla (EMOTIONAL)",
+    videoUploader: "Dave Doughbrick",
+    dateVideoUploaded: "11-02-2022",
+    videoViews: 300000
+  },
+  {
+    thumbnailImg: "https://via.placeholder.com/600x400",
+    videoTitle: "React in 100 seconds",
+    videoUploader: "Fireship",
+    dateVideoUploaded: "2022-09-08",
+    videoViews: 255
+  }
+];
 
   return (
     <div className="app-container">
-      {listOfViews.map((e) => {
+      {sampleData.map((e) => {
         return (
         <YoutubeComponent thumbnailImg={e.thumbnailImg} 
         videoTitle={e.videoTitle} 
-        uploader={e.uploader}
-        videouploaded={e.videouploaded}
-        views={e.views}  />
+        uploader={e.videoUploader}
+        videouploaded={e.dateVideoUploaded}
+        views={e.videoViews}  />
         )})}
     </div>
   )
